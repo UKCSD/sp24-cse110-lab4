0ad324a5dd196372b869598e09350bcabb9fe0c4
UKGithub <147003715+UKCSD@users.noreply.github.com>
2024-04-30 19:27:57 -0700

    Update part2-question16.js

--- expose/javascript/part2-question16.js
+++ expose/javascript/part2-question16.js
@@ -1,5 +1,7 @@
-for (var name in statistics) {
-    if (statistics[name] % 2 != 0 || name[0] == 'r') {
+for (var name in statistics) 
+{
+    if (statistics[name] % 2 != 0 || name[0] == 'r') 
+    {
         console.log(statistics[name]);
     }
 }
